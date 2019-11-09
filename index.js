305307f837059a4a415a528759e7c32aeb209ce1
liu zhengdong <735083049@qq.com>
2019-11-09 22:36:30 +0800

    change_normal

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 var nodemailer = require('nodemailer')
+const https = require('https')
+
+https.get("https://www.baidu.com",(res)=>{
+    let rawData = ""
+
+    res.on("error",(err)=>{
+        console.log(err)
+    }
+    )
+    res.on("data",(chunk)=>{
+        rawData+=chunk.toString('utf8')
+        console.log("下载中....")
+        //console.log(chunk.toString('utf8'))
+    })
+    res.on('end',()=>{
+        console.log(rawData)
+    })
+})
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+
+app.get('/user/login',(req,res)=>{
+let query = req.query
+
+if(query.us==='?'&&query.ps=='?'){
+    res.send({"err":0,"msg":"login ok"})
+    
+}else{
+    res.send({"err":-1,"msg":"login not ok"})
+}
+res.send("请求成功")
+})
+
+app.post('/usr/reg',(req,res)=>{
+    
+})
+
+
+app.listen(3000,()=>{
+    console.log('server start')
+})
